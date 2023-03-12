260cb59b5f91f963ed92a0a38ca7f2e0a7862f28
Juan Pablo Rodriguez Becerra <juanrdrz16@gmail.com>
2023-03-12 00:43:17 -0500

    Added GlideJS to the project

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
+//Glide JS
+import Glide from "../node_modules/@glidejs/glide/dist/glide.esm.js";
+
 const apiKey = "5592a035fe9f246cb15ff75906b34918";
-const trendingMovies = document.querySelector("#trending-movies");
+const trendingMoviesList = document.querySelector("#trending-movies-list");
+const upcomingMoviesList = document.querySelector("#upcoming-movies-list");
 
 document.addEventListener("DOMContentLoaded", () => {
   loadTrending();
-  //   loadAction();
+  loadUpcoming();
   //   loadAdventure();
   //   loadComedy();
 });
@@ -13,32 +17,52 @@ function loadTrending() {
   fetch(url)
     .then((response) => response.json())
     .then((movies) => {
-      displayMovies(movies.results, trendingMovies);
+      displayMovies(movies.results, trendingMoviesList);
+    })
+    .catch((error) => console.log(error));
+}
+
+function loadUpcoming() {
+  const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((movies) => {
+      displayMovies(movies.results, upcomingMoviesList);
     })
     .catch((error) => console.log(error));
 }
 
 function displayMovies(movies, ref) {
   console.log(movies);
+
   movies.forEach((movie) => {
     const { backdrop_path, title, release_date } = movie;
+
+    const divContainer = document.createElement("div");
+    divContainer.classList.add("relative", "h-[300px]", "rounded");
+
+    const li = document.createElement("li");
+    li.classList.add("glide__slide");
+
     const divMovieCard = document.createElement("div");
-    divMovieCard.classList.add(
-      "max-w-fit",
-      "rounded",
-      "overflow-hidden",
-      "shadow-lg"
-    );
+    divMovieCard.classList.add("w-full", "rounded", "h-[300px]", "rounded");
 
     const movieImg = document.createElement("img");
     movieImg.src = `https://image.tmdb.org/t/p/w200/${backdrop_path}`;
     movieImg.alt = title;
+    movieImg.classList.add("rounded", "h-1/2");
 
     const textContainerDiv = document.createElement("div");
-    textContainerDiv.classList.add("px-6", "py-4");
+    textContainerDiv.classList.add("py-4");
 
     const titleDiv = document.createElement("div");
-    titleDiv.classList.add("font-bold", "text-xl", "mb-2", "text-red-400");
+    titleDiv.classList.add(
+      "font-bold",
+      "text-lg",
+      "mb-2",
+      "text-red-400",
+      "text-left"
+    );
     titleDiv.textContent = title;
 
     const releaseDateP = document.createElement("p");
@@ -51,6 +75,20 @@ function displayMovies(movies, ref) {
     divMovieCard.appendChild(movieImg);
     divMovieCard.appendChild(textContainerDiv);
 
-    ref.appendChild(divMovieCard);
+    divContainer.appendChild(divMovieCard);
+
+    li.appendChild(divContainer);
+
+    ref.appendChild(li);
   });
+  loadGlide();
+}
+
+function loadGlide() {
+  new Glide(".glide", {
+    type: "carousel",
+    startAt: 0,
+    perView: 5,
+    gap: 20,
+  }).mount();
 }
